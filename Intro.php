80a2dc4cc6f7303e3141231fa385c8ce3f156e47
LyzzaP <mdp9104@students.uc-bcf.edu.ph>
2023-10-03 07:54:06 +0800

    Activities Commit

---
+++ Intro.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Introduction</title>
+</head>
+<body>
+ <?php 
+ echo"<h1>PHP Introduction</h1>";
+
+ echo "<h2>Hello, World!</h2>";
+ echo"PHP is a server side scripting language that is embedded in HTML. <br>It is used to manage 
+ dynamic content, databases, session tracking, even build entire e-commerce sites.<br>";
+
+echo"PHP supports a large number of major protocols such as POP3, IMAP, and LDAP. PHP4<br>
+added support for Java and distributed object architectures (COM and CORBA), making <br>
+n-tier development a possibility for the first time.";
+ $txt = "PHP";
+echo "<br>I love $txt! kimiii";
+ ?>
+
+
+</body>
+</html>
